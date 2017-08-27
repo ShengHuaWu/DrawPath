c4ee613bc2b4a53307abb2c0165bad87ee970de8
ShengHua Wu <fantasy0404@gmail.com>
2017-08-27 10:32:26 +0100

    Create imperative drawing functions.

--- DeclarativeDrawPath.playground/Sources/Drawing.swift
+++ DeclarativeDrawPath.playground/Sources/Drawing.swift
@@ -4,20 +4,20 @@ import UIKit
 extension CAShapeLayer {
     public static func rectangle(roundedRect: CGRect, cornorRadius: CGFloat, color: UIColor) -> CAShapeLayer {
         let path = UIBezierPath(roundedRect: roundedRect, cornerRadius: cornorRadius)
-        let layer = CAShapeLayer()
-        layer.path = path.cgPath
-        layer.fillColor = color.cgColor
+        let shape = CAShapeLayer()
+        shape.path = path.cgPath
+        shape.fillColor = color.cgColor
         
-        return layer
+        return shape
     }
     
     public static func oval(in rect: CGRect, color: UIColor) -> CAShapeLayer {
         let path = UIBezierPath(ovalIn: rect)
-        let layer = CAShapeLayer()
-        layer.path = path.cgPath
-        layer.fillColor = color.cgColor
+        let shape = CAShapeLayer()
+        shape.path = path.cgPath
+        shape.fillColor = color.cgColor
         
-        return layer
+        return shape
     }
     
     public static func triangle(_ point1: CGPoint, _ point2: CGPoint, _ point3: CGPoint, color: UIColor) -> CAShapeLayer {
@@ -27,11 +27,11 @@ extension CAShapeLayer {
         path.addLine(to: point3)
         path.close()
         
-        let layer = CAShapeLayer()
-        layer.path = path.cgPath
-        layer.fillColor = color.cgColor
+        let shape = CAShapeLayer()
+        shape.path = path.cgPath
+        shape.fillColor = color.cgColor
         
-        return layer
+        return shape
     }
     
     public static func line(start: CGPoint, end: CGPoint, width: CGFloat, color: UIColor) -> CAShapeLayer {
@@ -39,12 +39,12 @@ extension CAShapeLayer {
         path.move(to: start)
         path.addLine(to: end)
         
-        let layer = CAShapeLayer()
-        layer.path = path.cgPath
-        layer.lineWidth = width
-        layer.strokeColor = color.cgColor
+        let shape = CAShapeLayer()
+        shape.path = path.cgPath
+        shape.lineWidth = width
+        shape.strokeColor = color.cgColor
         
-        return layer
+        return shape
     }
 }
 
@@ -53,5 +53,50 @@ extension UIView {
         for shape in shapes {
             layer.addSublayer(shape)
         }
+    }
+    
+    public func drawRectangle(roundedRect: CGRect, cornorRadius: CGFloat, color: UIColor) {
+        let path = UIBezierPath(roundedRect: roundedRect, cornerRadius: cornorRadius)
+        let shape = CAShapeLayer()
+        shape.path = path.cgPath
+        shape.fillColor = color.cgColor
+        
+        layer.addSublayer(shape)
+    }
+    
+    public func drawOval(in rect: CGRect, color: UIColor) {
+        let path = UIBezierPath(ovalIn: rect)
+        let shape = CAShapeLayer()
+        shape.path = path.cgPath
+        shape.fillColor = color.cgColor
+        
+        layer.addSublayer(shape)
+    }
+    
+    public func drawTriangle(_ point1: CGPoint, _ point2: CGPoint, _ point3: CGPoint, color: UIColor) {
+        let path = UIBezierPath()
+        path.move(to: point1)
+        path.addLine(to: point2)
+        path.addLine(to: point3)
+        path.close()
+        
+        let shape = CAShapeLayer()
+        shape.path = path.cgPath
+        shape.fillColor = color.cgColor
+        
+        layer.addSublayer(shape)
+    }
+    
+    public func drawLine(start: CGPoint, end: CGPoint, width: CGFloat, color: UIColor) {
+        let path = UIBezierPath()
+        path.move(to: start)
+        path.addLine(to: end)
+        
+        let shape = CAShapeLayer()
+        shape.path = path.cgPath
+        shape.lineWidth = width
+        shape.strokeColor = color.cgColor
+        
+        layer.addSublayer(shape)
     }
 }
