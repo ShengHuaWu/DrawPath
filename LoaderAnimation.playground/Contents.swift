3070bb09b703dccb072b4e842e9ef2a01218709b
ShengHua Wu <fantasy0404@gmail.com>
2017-09-01 12:49:15 +0100

    Implement loader view detail.

--- LoaderAnimation.playground/Contents.swift
+++ LoaderAnimation.playground/Contents.swift
@@ -5,6 +5,8 @@ let containFrame = CGRect(x: 0, y: 0, width: 500, height: 500)
 let containView = UIView(frame: containFrame)
 containView.backgroundColor = .white
 
-//let loader = CircularLoaderView(frame: .zero)
+let loaderFrame = containFrame.insetBy(dx: 100, dy: 100)
+let loader = CircularLoaderView(frame: loaderFrame)
+containView.addSubview(loader)
 
 PlaygroundPage.current.liveView = containView
--- LoaderAnimation.playground/Sources/CircularLoaderView.swift
+++ LoaderAnimation.playground/Sources/CircularLoaderView.swift
@@ -1,5 +1,43 @@
 import UIKit
 
 public final class CircularLoaderView: UIView {
+    private let circlePathLayer = CAShapeLayer()
+    public let circleRadius: CGFloat = 20.0
     
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        configure()
+    }
+    
+    func configure() {
+        circlePathLayer.frame = bounds
+        circlePathLayer.lineWidth = 2
+        circlePathLayer.fillColor = UIColor.clear.cgColor
+        circlePathLayer.strokeColor = UIColor.red.cgColor
+        layer.addSublayer(circlePathLayer)
+        backgroundColor = .white
+    }
+    
+    func circleFrame() -> CGRect {
+        var circleFrame = CGRect(x: 0, y: 0, width: 2 * circleRadius, height: 2 * circleRadius)
+        let circlePathBounds = circlePathLayer.bounds
+        circleFrame.origin.x = circlePathBounds.midX - circleFrame.midX
+        circleFrame.origin.y = circlePathBounds.midY - circleFrame.midY
+        return circleFrame
+    }
+    
+    func circlePath() -> UIBezierPath {
+        return UIBezierPath(ovalIn: circleFrame())
+    }
+    
+    override public func layoutSubviews() {
+        super.layoutSubviews()
+        circlePathLayer.frame = bounds
+        circlePathLayer.path = circlePath().cgPath
+    }
 }
